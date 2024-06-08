ac5a762aba75144821d9014db16b72b0ab9613ad
kripto63 <ignatov.fedor.evgenivich@gmail.com>
2024-06-08 18:27:24 +0400

    create connection BD

--- route/user/data.js
+++ route/user/data.js
@@ -1,3 +1,31 @@
+import sqlite3 from 'sqlite3';
+const SQlite3 = sqlite3.verbose();
+
+// Подключение к БД
+let db = new SQlite3.Database('./route/user/data.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log('Подключение к SQLite базе данных выполнено успешно.');
+});
+
+
+console.log(db.run('CREATE TABLE USER IF NOT EXISTS ( ID NUMBER , NAME VARCHAR(100));', (err) => {
+    if (err) return;
+}))
+
+db.run("SELECT name FROM sqlite_master WHERE type='table';");
+
+// Закрытие подключения
+db.close((err) => {
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log('Соединение с базой данных закрыто.');
+});
+
+
+//
 let users = ["test", "test2"]
 let countUsers = 0;
 
